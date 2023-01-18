e856cbef27666c0a3ac562602bff2294cf45ef50
HyunAm0225 <41044429+HyunAm0225@users.noreply.github.com>
2023-01-18 15:12:40 +0900

    Feat : ledger model 생성

--- ledger/models.py
+++ ledger/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+from base.models import BaseModel
+
+
+class Ledger(BaseModel):
+    memo = models.TextField(verbose_name="메모", default=None, null=True)
+    amount = models.BigIntegerField(verbose_name="예산", default=0)
+    user = models.ForeignKey(
+        verbose_name="유저",
+        to="user.User",
+        on_delete=models.CASCADE,
+        related_name="ledger",
+    )
+    isActive = models.BooleanField(verbose_name="활성화 여부")
+
+    class Meta:
+        verbose_name = "회계"
